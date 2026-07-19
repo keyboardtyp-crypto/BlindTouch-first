cb2bc8fcd896a6bb55228c92be81e6c9ff3f9500
keyboard H <keyboard.typ@gmail.com>
2026-07-19 11:33:52 +0900

    fix: middlewaeをコメントにした

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+/*
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
@@ -49,3 +50,4 @@ export const config = {
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
+*/
