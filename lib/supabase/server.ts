697075d7ac82be12cf861e770c2ab8b538ddc9cc
keyboard H <keyboard.typ@gmail.com>
2026-07-30 17:39:40 +0900

    fix:cookieのテスト

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,8 +2,8 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
- // const cookieStore = await cookies()
-    const cookieStore = cookies()
+  const cookieStore = await cookies()
+    //const cookieStore = cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -14,10 +14,13 @@ export async function createClient() {
         },
         // 💡 cookiesToSet に : any[] を追加して型エラーを解消
         setAll(cookiesToSet: any[]) {
+
+console.log('--- setAll CALLED ---', cookiesToSet.length) // 👈 これを追加 test
+
           try {
             // 💡 引数の分割代入部分に : any を追加して暗黙のanyを解消
-            cookiesToSet.forEach(({ name, value, options }: any) =>
-              
+            cookiesToSet.forEach(({ name, value, options }: any) => {
+ console.log('Setting Cookie:', name) // 👈 これを追加 test             
               /*
               cookieStore.set(name, value, { 
                 ...options, 
@@ -27,9 +30,10 @@ export async function createClient() {
               })
               */
               cookieStore.set(name, value, options)
-            )
-          } catch {
+           } )
+          } catch (error){
             // サーバーアクション内での変更を許容するための catch ブロック
+            console.log('setAll Error:', error) // 👈 エラーが出ているかログ出力
           }
         },
       },
